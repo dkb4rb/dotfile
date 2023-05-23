0af3178c7b08fe58c9dded7e3c1b77b1f5870a7e
dkb4rb <jjuannca651@gmail.com>
2023-05-23 01:49:49 -0500

    Update dotfiles arch linux rise

---
+++ awesome/rc.lua
@@ -0,0 +1,54 @@
+--- ░█▀▄░█░█░█░█░█░█░█▀█░▀░█▀▀░░░█▀█░█░█░█▀▀░█▀▀░█▀█░█▄█░█▀▀
+--- ░█▀▄░▄▀▄░░█░░█▀█░█░█░░░▀▀█░░░█▀█░█▄█░█▀▀░▀▀█░█░█░█░█░█▀▀
+--- ░▀░▀░▀░▀░░▀░░▀░▀░▀░▀░░░▀▀▀░░░▀░▀░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀▀▀
+--- ~~~~~~~~~~~~~~~~~~  @author rxyhn ~~~~~~~~~~~~~~~~~~~~~~
+--- ~~~~~~~~~~~~ https://github.com/rxyhn ~~~~~~~~~~~~~~~~~~
+
+pcall(require, "luarocks.loader")
+local gears = require("gears")
+local beautiful = require("beautiful")
+local awful = require("awful")
+--- ░▀█▀░█░█░█▀▀░█▄█░█▀▀
+--- ░░█░░█▀█░█▀▀░█░█░█▀▀
+--- ░░▀░░▀░▀░▀▀▀░▀░▀░▀▀▀
+
+local theme_dir = gears.filesystem.get_configuration_dir() .. "theme/"
+beautiful.init(theme_dir .. "theme.lua")
+
+--- ░█▀▀░█▀█░█▀█░█▀▀░▀█▀░█▀▀░█░█░█▀▄░█▀█░▀█▀░▀█▀░█▀█░█▀█░█▀▀
+--- ░█░░░█░█░█░█░█▀▀░░█░░█░█░█░█░█▀▄░█▀█░░█░░░█░░█░█░█░█░▀▀█
+--- ░▀▀▀░▀▀▀░▀░▀░▀░░░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀░░▀░░▀▀▀░▀▀▀░▀░▀░▀▀▀
+
+require("configuration")
+
+--- ░█▄█░█▀█░█▀▄░█░█░█░░░█▀▀░█▀▀
+--- ░█░█░█░█░█░█░█░█░█░░░█▀▀░▀▀█
+--- ░▀░▀░▀▀▀░▀▀░░▀▀▀░▀▀▀░▀▀▀░▀▀▀
+
+require("modules")
+
+--- ░█░█░▀█▀
+--- ░█░█░░█░
+--- ░▀▀▀░▀▀▀
+
+require("ui")
+
+--- ░█▀▀░█▀█░█▀▄░█▀▄░█▀█░█▀▀░█▀▀
+--- ░█░█░█▀█░█▀▄░█▀▄░█▀█░█░█░█▀▀
+--- ░▀▀▀░▀░▀░▀░▀░▀▀░░▀░▀░▀▀▀░▀▀▀
+
+--- Enable for lower memory consumption
+collectgarbage("setpause", 110)
+collectgarbage("setstepmul", 1000)
+gears.timer({
+	timeout = 5,
+	autostart = true,
+	call_now = true,
+	callback = function()
+		collectgarbage("collect")
+	end,
+})
+-- wallpaper
+local wallpaper='feh --bg-fill /home/DK/Pictures/BackGround.jpg'
+awful.util.spawn_with_shell("picom --dbus --config /home/DK/.config/awesome/theme/picom.conf &")
+os.execute(wallpaper)
